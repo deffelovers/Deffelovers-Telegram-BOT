76e148e3aba73d48d194b43b19e871946a35cea7
deffelovers <deffelovers.code@gmail.com>
2022-08-18 04:47:00 +0700

    fungsi  batas download

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// konfigurasi env
+// konfigurasi env.
 require('dotenv').config({path: 'conf.txt'});
 
-// konfigurasi komponen & telegram bot
+// konfigurasi komponen & telegram bot.
 const {Telegraf} = require('telegraf'),
 bot = new Telegraf(process.env.BOT_TOKEN),
 env = require('./lib/utils/env'),
@@ -12,18 +12,41 @@ chat = require('./chat.js');
 // cache
 const thisBot = {
 
-    // berisi nilai status dari bot (seperti on/off)
+    // berisi nilai status dari bot (seperti true/false).
     status: process.env.isBotOn,
 
-    // sebuah fungsi untuk mengubah nilai 'isBotOn' dari conf.txt
+    // sebuah fungsi untuk mengubah nilai 'isBotOn' dari conf.txt.
     setStatus: (boolean) => env('isBotOn', boolean)
 },
-isDownloaded = [];
+Downloaded = {
+
+    // berisi nilai pengguna yang sedang mengunduh.
+    list: [1, 2, 12],
+
+    /*
+        Sebuah fungsi untuk menambahkan pengguna yang sedang mengunduh,
+        fungsi ini memblokir pengguna agar tidak mengunduh lebih dari 1x,
+        untuk mencegah bentrokan unduhan yang dapat membuat bot error.
+    */
+    add: function(usrId){
+        return this.list.length > 0
+            ? this.list.filter(x => x === usrId)[0]
+                ?? this.list.push(usrId)
+        : this.list.push(usrId)
+    },
+
+    /*
+        Sebuah fungsi untuk menghapus pengguna yang sedang mengunduh,
+        agar pengguna bisa mengunduh file kembali.
+    */
+    remove: function(usrId){
+        return this.list.indexOf(usrId)
+            ?? this.list.splice(this.list.indexOf(usrId), 1)
+    }
+};
 
 
-// pengolah pesan
-bot.on('message', (ctx) => chat.inMessage(ctx, bot))
+// pengolah pesan.
+bot.on('message', (ctx) => chat.inMessage(ctx, bot, Downloaded))
 
-bot.launch()
-// thisBot.setStatus(true)
-// console.log(thisBot.status);
+// bot.launch()
--- chat.js
+++ chat.js
@@ -1,41 +1,41 @@
 // pustaka lokal
 // const {} = require()
 
-// list perintah
+// list perintah.
 const {start} = require('./lib/utils/commandList');
 
-// modul pengolah pesan
+// modul pengolah pesan.
 module.exports = {
-    inMessage: async (ctx, bot) => {
+    inMessage: async (ctx, bot, Downloaded) => {
 
-        // info bot
+        // info bot.
         const botInfo = ctx.botInfo,
         botId = botInfo.id,
         botName = botInfo.first_name,
         botUserName = botInfo.username;
 
-        // tipe media
+        // tipe media.
         const photo = ctx.message.photo,
         video = ctx.message.video,
         file = ctx.message.document,
         caption = ctx.message.caption;
         
-        // cek tipe pesan
+        // cek tipe pesan.
         const isprivate = ctx.message.chat.type === 'private',
         isadmin = ctx.message.chat.type === 'supergroup';
 
-        // filter pesan
+        // filter pesan.
         const text = ctx.message.text,
         cmd = text?.split(' ')[0].toLowerCase();
 
-        // aksi chat
+        // aksi chat.
         const typing = () => bot.telegram.sendChatAction(ctx.chat.id, 'typing'),
         upPhoto = () => bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo'),
         upVideo = () => bot.telegram.sendChatAction(ctx.chat.id, 'upload_video'),
         upVoice = () => bot.telegram.sendChatAction(ctx.chat.id, 'upload_voice'),
         upDocument = () => bot.telegram.sendChatAction(ctx.chat.id, 'upload_document');
 
-        // pengolahan pesan
+        // pengolahan pesan.
         cmd === '/start' || cmd === `/start${botName}`
             ? typing() + ctx.reply(start(botName, '4.0'))
         : ''
