ffc4122443e9dd06a5eeb1cecf0abb8310d1a7b8
deffelovers <deffelovers.code@gmail.com>
2022-08-17 08:50:50 +0700

    kita rehat sejenak

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 // konfigurasi env
 require('dotenv').config({path: 'conf.txt'});
 
-// cache
-const isBotOn = [],
-isDownloaded = [];
-
 // konfigurasi komponen & telegram bot
 const {Telegraf} = require('telegraf'),
 bot = new Telegraf(process.env.BOT_TOKEN),
-
-// konfigurasi pesan
+env = require('./lib/utils/env'),
+//
 chat = require('./chat.js');
+
+
+// cache
+const thisBot = {
+
+    // berisi nilai status dari bot (seperti on/off)
+    status: process.env.isBotOn,
+
+    // sebuah fungsi untuk mengubah nilai 'isBotOn' dari conf.txt
+    setStatus: (boolean) => env('isBotOn', boolean)
+},
+isDownloaded = [];
+
 
 // pengolah pesan
 bot.on('message', (ctx) => chat.inMessage(ctx, bot))
 
-// bot.launch()
+bot.launch()
+// thisBot.setStatus(true)
+// console.log(thisBot.status);
